f7bc641b24460170fe6192e3e36effa29d5c49f0
luis <hola@luisespejo.com>
2023-11-10 16:59:07 -0500

    js del botón de inicio de sesión

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const buttonsContainer = document.querySelector('.buttonsContainer');
+let formLoging = document.querySelector('.formLoging');
+
+buttonsContainer.addEventListener('click', (e) =>{
+    if(e.target.classList.contains('login')){
+        formLoging.classList.toggle('active');
+    }
+    else if(e.target.classList.contains('signUp')){
+        console.log('botón de registro');
+    }
+});
+
