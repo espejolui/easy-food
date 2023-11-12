a93ac9804d72cf957327f794a3d9285737f3ac5a
luis espejo <hola@luisespejo.com>
2023-11-11 23:21:39 -0500

    async, defer y clases actalizadas

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 const buttonsContainer = document.querySelector('.buttonsContainer');
 let formLoging = document.querySelector('.formLoging');
 
-buttonsContainer.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('login')){
+buttonsContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('login')) {
         formLoging.classList.toggle('active');
     }
-    else if(e.target.classList.contains('signUp')){
+    else if (e.target.classList.contains('signUp')) {
         console.log('botón de registro');
     }
 });
